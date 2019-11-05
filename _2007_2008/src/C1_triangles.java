89f9729ab248955e823710a6f6cd554e3b1dee7c
rlaalsdn <brandon5681@icloud.com>
2019-11-04 20:21:38 -0800

    error with triangles program

---
+++ _2007_2008/src/C1_triangles.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class C1_triangles {
+
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(new File("C1JR.in"));
+		double[] a = new double[6];
+		for (int i = 1; i<=10; i++) {
+			int count = 0;
+			
+			//input
+			String[] str = in.nextLine().split(" ");
+			System.out.println(Arrays.toString(str));
+			
+			for (int k = 0; k<3; k++) {
+				System.out.print("[" + k + " " + str[k] + "] ");
+				for (int j = 3; j<6; j++) {
+					System.out.print("(" + j + " " + str[j] + ") ");
+					if (str[k] == str[j]) {
+						System.out.println(str[k] + " " + str[j]);
+						count = count + 1;
+					}
+				}
+			}
+			System.out.println(count);
+			
+			
+		}
+		
+	}
+
+}
